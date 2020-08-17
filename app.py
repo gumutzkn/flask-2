78e49bc9c89ee9d280b8e1c0b118046cd7dac3e0
Görkem <gumutzkn@gmail.com>
2020-08-17 16:25:12 +0300

    auth

--- app.py
+++ app.py
@@ -1,26 +1,92 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
+from flask import (
+    Flask,
+    render_template,
+    request,
+    url_for,
+    redirect,
+    json,
+    session
+)
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.secret_key = "thisissecretkey"
 
-todos = []
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 
 @app.route("/")
-def tasks():
-    if "todos" not in session:
-        session["todos"] = []
-    return render_template("tasks.html", todos=session["todos"])
+def index():
+    return render_template("base.html")
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
+@app.route("/users")
+def get_users():
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "GET":
-        return render_template("add.html")
+        return render_template("login.html")
     else:
-        todo = request.form.get("task")
-        session["todos"].append(todo)
-        return redirect("/")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return {"message": "There is no user with that email."}
+
+        if user.password != password:
+            return {"message": "Password is incorrect!"}
+
+        session["username"] = user.username
+
+        return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        new_user = User(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        # add the new_user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run()
